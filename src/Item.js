ebc5f6e3b40fa96403a1910265ff5f506a4d10c0
rkOSU <46794263+rkOSU@users.noreply.github.com>
2020-12-03 16:26:10 -0500

    Initial Commit

---
+++ src/Item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css'
+function Item(){
+    return(
+        <div className="item">
+            <h4>Hi Maddie</h4>
+        </div>
+    )
+}
+
+export default Item;
